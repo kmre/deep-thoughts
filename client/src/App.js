72dbd22dceeaadfe1924fc9bf2357c2a9e5a2b7a
kmre <karla.romero7@gmail.com>
2021-10-19 15:57:09 -0400

    git ignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
